162830544cf04aa3c7451038141fc84c98541b0a
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-06 00:36:50 -0700

    finished hero section

--- ZohaibFattani/src/components/Hero/Hero.js
+++ ZohaibFattani/src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My purpose is to secure a challenging position in a reputable organization to expand my learnings, knowledge, and skills. Secure a responsible career opportunity to fully utilize my training, skills, and past years of experience, while making a significant contribution to the success of the company.
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://github.com/ZohaibFattani'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
